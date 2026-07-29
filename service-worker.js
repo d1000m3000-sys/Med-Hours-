1367110fa78b79f38be43c1a009d058e68246956
d1000m3000-sys <d1000m3000@gmail.com>
2026-07-29 19:03:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,101 +1,58 @@
-const CACHE_NAME = "med-hours-v1";
-
-
-const FILES_TO_CACHE = [
-
-    "./",
-
-    "./index.html",
-
-    "./style.css",
-
-    "./script.js",
-
-    "./manifest.json"
-
+const CACHE_NAME = 'med-hours-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap'
 ];
 
-
-
-// تثبيت التطبيق وحفظ الملفات
-self.addEventListener("install", event => {
-
-
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-
-        .then(cache => {
-
-            return cache.addAll(FILES_TO_CACHE);
-
-        })
-
-    );
-
-
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
 });
 
-
-
-
-// تشغيل التطبيق من الذاكرة
-self.addEventListener("fetch", event => {
-
-
-    event.respondWith(
-
-
-        caches.match(event.request)
-
-        .then(response => {
-
-
-            return response || fetch(event.request);
-
-
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-
-
-    );
-
-
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-
-
-
-// تحديث النسخة القديمة
-self.addEventListener("activate", event => {
-
-
-    event.waitUntil(
-
-        caches.keys().then(keys => {
-
-
-            return Promise.all(
-
-                keys.map(key => {
-
-
-                    if (key !== CACHE_NAME) {
-
-
-                        return caches.delete(key);
-
-
-                    }
-
-
-                })
-
-            );
-
-
-        })
-
-    );
-
-
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return networkResponse;
+      }).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
 });
+
